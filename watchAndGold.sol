f8e3f50f026917faef99305d4350e94b55f13b5a
Praveen Patawkar <107745548+Praveen2645@users.noreply.github.com>
2023-09-29 17:52:59 +0530

    DICproposalForWatch.sol

--- watchAndGold.sol
+++ watchAndGold.sol
@@ -1,111 +1,145 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.20;
 
-import { ERC721 } from "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
-import { IERC721Receiver } from "openzeppelin-contracts/contracts/token/ERC721/IERC721Receiver.sol";
-import { getInvestorProposal } from "./getInvestorProposal.sol";
+// import {ERC721} from "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+// import {IERC721Receiver} from "openzeppelin-contracts/contracts/token/ERC721/IERC721Receiver.sol";
 
-contract CollateralizedAuction is ERC721 {
-    error NotOwner();
-    
-    struct CollateralDetails {
-        uint256 collateralId;
-        uint256 collateralValue;
-        address collateralOwner;
-        bool isApproved;
-    }
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "../Inventory/Struct.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "./InvestorProposal.sol";
+import "../Admin.sol";
+import "../Inventory/Struct.sol";
+//import {DIC} from "../DIC.sol";
 
-    struct AuctionItem {
-        uint256 itemId;
-        string itemName;
-        uint256 itemValue;
-        address itemOwner;
-        uint256 auctionDuration;
-        bool onAuction;
-        uint256 collateralId; // ID of the associated collateral
-    }
+                                
+contract DICpropsalForWatch is ERC721{
+
+
+    ///////////////////////
+      //State Variable //
+    //////////////////////
 
     address owner;
-    uint256 itemId;
-    uint256 collateralId;
-    
-    mapping(uint256 => AuctionItem) public auctionItems;
-    mapping(uint256 => CollateralDetails) public collateralDetails;
+    address adminAddress;
+    uint256 watchId;
+    watchDetails details;
 
-    event ItemListedForAuction(
+    mapping(uint256 wactchId => watchDetails) public watchIdTowatchDetails;
+
+    //////////////////
+      //Events //
+    /////////////////
+
+     
+
+    event ProposalContractCreated(
         address indexed owner,
-        uint256 indexed itemId,
-        string itemName,
-        uint256 itemValue,
-        uint256 auctionDuration,
-        uint256 collateralId
+        uint256 indexed watchId,
+        address indexed InvestorProposalContract
     );
 
-    event AuctionContractCreated(
-        address indexed owner,
-        uint256 indexed itemId,
-        address indexed auctionContract
-    );
-
-    constructor() ERC721("CollateralizedNFT", "CNFT") {
+    constructor(address _admin) ERC721("WatchNFT", "WATCH") {
         owner = msg.sender;
+        adminAddress = _admin;
     }
 
-    modifier onlyOwner() {
-        if (owner != msg.sender) {
-            revert NotOwner();
-        }
-        _;
-    }
+  modifier onlyAdmin() {
+    require(msg.sender == adminAddress, "Only admin can call this function.");
+    _;
+}
 
-    function mintNFT(address _to, uint256 _tokenId) internal returns (bool) {
-        _safeMint(_to, _tokenId);
+
+
+    ////////////////////
+       // Functions //
+    ////////////////////
+
+    function mintNFT(address _to, uint256 _watchId) internal returns (bool) {
+        _safeMint(_to, _watchId);
         return true;
     }
 
-    function createAuction(
-        string memory _itemName,
-        uint256 _itemValue,
-        uint256 _auctionDuration,
-        uint256 _collateralValue,
-        bool _isApproved
-    ) external onlyOwner returns (address) {
-        itemId++;
-        collateralId++;
+
+  
+
+    function setWatchForProposal(
+        string memory _watchBrand,
+        string memory _modelName,
+        uint256 _assetId,
+        uint256 _assetCategoryId,
+        uint256 _investmentId,
+        uint256 _EstimatedValue,
+        uint256 _minRangeOfLoanAmount,
+        uint256 _maxRangeOfLoanAmount,
+        uint256 _minRangeOfLoanDuration,
+        uint256 _maxRangeOfLoanDuration,
+        uint256 _minRangeOfInterestRate,
+        uint256 _maxRangeOfInterestRate,
+        uint256 _loanDuration,
+        bool onAuction,
+        address _borrower
+
+    ) external onlyAdmin() returns (address) {
+        watchId++;
+        details.watchId= watchId;
+        details.watchBrand = _watchBrand;
+        details.modelName = _modelName;
+        details.assetId = _assetId;
+        details.assetCategoryId = _assetCategoryId;
+        details.investmentId = _investmentId;
+        details.EstimatedValue = _EstimatedValue;
+        details.minRangeOfLoanAmount = _minRangeOfLoanAmount;
+        details.maxRangeOfLoanAmount = _maxRangeOfLoanAmount;
+        details.minRangeOfLoanDuration = _minRangeOfLoanDuration;
+        details.maxRangeOfLoanDuration = _maxRangeOfLoanDuration;
+        details.minRangeOfInterestRate = _minRangeOfInterestRate;
+        details.maxRangeOfInterestRate = _maxRangeOfInterestRate;
+        details.loanDuration = _loanDuration;
+        details.onAuction = true;
+        details.borrower = _borrower;
+
         
-        // Mint an NFT for the auction item
-        mintNFT(msg.sender, itemId);
+        
+        mintNFT(msg.sender, watchId);
+        // emit ItemListedForAuction(
+        //     msg.sender,
+        //     watchNumber,
+        //     _watchName,
+        //     _watchPrice,
+        //     _askingPrice,
+        //     _borrowDuration,
+        //     _auctionDuration
+        // );
 
-        // Create a new auction item
-        AuctionItem storage newItem = auctionItems[itemId];
-        newItem.itemId = itemId;
-        newItem.itemName = _itemName;
-        newItem.itemValue = _itemValue;
-        newItem.itemOwner = msg.sender;
-        newItem.auctionDuration = _auctionDuration;
-        newItem.onAuction = true;
-        newItem.collateralId = collateralId;
-
-        // Create a new collateral
-        CollateralDetails storage newCollateral = collateralDetails[collateralId];
-        newCollateral.collateralId = collateralId;
-        newCollateral.collateralValue = _collateralValue;
-        newCollateral.collateralOwner = msg.sender;
-        newCollateral.isApproved = _isApproved;
-
-        emit ItemListedForAuction(msg.sender, itemId, _itemName, _itemValue, _auctionDuration, collateralId);
-
-        getInvestorProposal proposalContract = new getInvestorProposal(
-            itemId,
-            _itemName,
-            _itemValue,
-            _auctionDuration,
-            collateralId
+        InvestorProposal getProposal = new InvestorProposal(
+            _watchBrand,
+            _modelName,
+             watchId,
+            _assetId,
+            _assetCategoryId,
+            _investmentId,
+            _EstimatedValue,
+            _minRangeOfLoanAmount,
+            _maxRangeOfLoanAmount,
+            _minRangeOfLoanDuration,
+            _maxRangeOfLoanDuration,
+            _minRangeOfInterestRate,
+            _maxRangeOfInterestRate,
+            _loanDuration,
+            true,
+            _borrower
+            
         );
-
-        emit AuctionContractCreated(msg.sender, itemId, address(proposalContract));
-
-        return address(proposalContract);
+       
+        watchIdTowatchDetails[watchId] = details;
+        //emit AuctionContractCreated(msg.sender, watchNumber, address(getProposal));
+        return address(getProposal);
+       
     }
+
 }
+
+
